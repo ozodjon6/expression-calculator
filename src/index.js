7d33b44093e510442d6b4f3f2e1794f677faeff9
Ozodbek <ozodjon6@gmail.com>
2021-07-08 18:28:44 +0500

    feat: add expression Calculator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/\s/g, '');
+    if(expr.indexOf('/0') > 0) throw new Error('TypeError: Division by zero.');
+
+    try {
+        let res = new Function('return '+ expr);
+        return res();
+    }
+
+    catch {
+        throw new Error('ExpressionError: Brackets must be paired')
+    }
 }
 
 module.exports = {
